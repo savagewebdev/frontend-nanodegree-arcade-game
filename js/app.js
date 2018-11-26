6f4be285c4d196417e53e6e5b2c5898885378e78
Robin Savage <macbookbro@Robins-MacBook-Pro.local>
2018-11-26 17:58:56 -0600

    Refactored objects to classes

--- js/app.js
+++ js/app.js
@@ -1,39 +1,40 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor(sprite = 'images/enemy-bug.png') {
-    this.sprite = sprite;
+    constructor(sprite = 'images/enemy-bug.png', enemies = 3) {
+        this.sprite = sprite;
+    }
+    
+    update(dt) {
     }
 
-    function update(dt) { 
-        
-    };
-
-    function render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
-
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
-    this.sprite = 'images/char-boy.png';
-};
+class Player {
+    constructor(sprite = 'images/char-boy.png') {
+    this.sprite = sprite;
+    }
+    
+    update(dt) {
+    }
 
-Player.prototype.update = function(dt) {
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function() {
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    
+    handleInput() {
+    }
+    
+}
 
 // Now instantiate your objects.
-Enemy();
-Player();
+const Feind = new Enemy();
+const Spieler = new Player();
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 var feind = new Enemy();
