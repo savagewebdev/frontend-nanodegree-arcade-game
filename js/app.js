4aa8eddef5d3f0ad097a826a7b937bab65adb490
Robin Savage <macbookbro@Robins-MacBook-Pro.local>
2018-12-02 19:35:14 -0600

    Thus far
    Included code to allow a tentative movement on the game board.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor(x, y) {
+    constructor(x, y, speed) {
         this.x = x;
         this.y = y;
         this.sprite = 'images/enemy-bug.png';
+        this.speed = speed;
     }
     
     update(dt) {
+        this.x = this.x + this.speed * dt;
+        if(this.x > 505){this.x = 0;} // If the Enemy scrolls off-screen, then reset position.
     }
-
+    
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+    
 }
 // Now write your own player class
 // This class requires an update(), render() and
@@ -31,30 +35,48 @@ class Player {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     
-    handleInput() {
+    handleInput(allowedKeys) {
+        let z = allowedKeys;
+        
+        if (z === 'left') { // Left
+            this.x + 85;
+        }
+        
+        if (z === 'up') { // Up
+            this.y + 85;
+        }
+        
+        if (z === 'right') { // Right
+            this.x - 85;
+        }
+        
+        if (z === 'right') { // Down
+            this.y - 85;
+        }
     }
-    
 }
+
 
 var allEnemies = [];
 let rowPos = 60; // Vertical
+let speed = 300; // Starting speed test
 
 for (let num = 1; num <= 3; num++){
-    let Feind = new Enemy(5, rowPos)
+    let Feind = new Enemy(5, rowPos, speed)
     allEnemies.push(Feind);
     rowPos += 85;
+    speed -= 50;
 }
 
 //const Spieler = new Player;
 
 // Place the player object in a variable called player
 const player = new Player(203, 425);
-Feind.render();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
