bb4f3a1438fe09515063fd1ede2742e76235d55b
Robin Savage <macbookbro@Robins-MacBook-Pro.local>
2018-12-09 18:10:17 -0600

    Sprite collision to be worked on
    https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor(x, y, speed) {
+    constructor(x, y, speed, spriteWidth, spriteHeight) {
         this.x = x;
         this.y = y;
         this.sprite = 'images/enemy-bug.png';
         this.speed = speed;
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+        this.bound = spriteWidth * spriteHeight;
+        
     }
     
     update(dt) {
@@ -22,10 +26,13 @@ class Enemy {
 // a handleInput() method.
 
 class Player {
-    constructor(x, y) {
+    constructor(x, y, spriteWidth, spriteHeight) {
         this.x = x; // Horizontal
         this.y = y; // Vertical
         this.sprite = 'images/char-boy.png'
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+        this.bound = spriteWidth * spriteHeight
     }
     
     update(dt) {
@@ -39,19 +46,19 @@ class Player {
         let z = allowedKeys;
         
         if (z === 'right') { // Right
-            this.x += 85;
+            this.x += 101;
         }
         
         if (z === 'down') { // Down
-            this.y += 85;
+            this.y += 83;
         }
         
         if (z === 'left') { // Left
-            this.x -= 85;
+            this.x -= 101;
         }
         
         if (z === 'up') { // Up
-            this.y -= 85;
+            this.y -= 83;
         }
     }
 }
@@ -62,16 +69,18 @@ let rowPos = 60; // Vertical
 let speed = 300; // Starting speed test
 
 for (let num = 1; num <= 3; num++){
-    let Feind = new Enemy(5, rowPos, speed)
+    let Feind = new Enemy(5, rowPos, speed, 98, 66) // 98 x-horizontal 66 y-vertical
     allEnemies.push(Feind);
-    rowPos += 85;
+    rowPos += 83;
     speed -= 50;
 }
 
 //const Spieler = new Player;
 
 // Place the player object in a variable called player
-const player = new Player(203, 425);
+const player = new Player(205, 405, 68, 75); //68 x-horizontal 75-vertical
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
